3a27aaeed4580f2cb51e1d2198d4eea285b4988e
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-10-08 22:56:15 -0400

    Add folder controller

---
+++ controllers/folderController.js
@@ -0,0 +1,34 @@
+const asyncHandler = require("express-async-handler");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+const createFolder = asyncHandler(async (req, res) => {
+  await prisma.folder.create({
+    data: {
+      name: req.body.name,
+      userId: req.user.id,
+    },
+  });
+  res.redirect("/folders");
+});
+
+const getFolders = asyncHandler(async (req, res) => {
+  if (req.isAuthenticated()) {
+    const folders = await prisma.folder.findMany({
+      where: {
+        userId: {
+          equals: req.user.id,
+        },
+      },
+    });
+
+    res.render("folder/folders", { folders });
+  } else {
+    res.redirect("/");
+  }
+});
+
+module.exports = {
+  createFolder,
+  getFolders,
+};
