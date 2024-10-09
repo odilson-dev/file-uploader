c1ce83e898b19177f3fe2e3ec9eede9165dae0b6
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-10-08 23:46:21 -0400

    Add corresponding routes to match edit and update actions

--- routes/folders.js
+++ routes/folders.js
@@ -5,6 +5,9 @@ var folderController = require("../controllers/folderController");
 /* GET users listing. */
 router.get("/", folderController.getFolders);
 
-router.post("/new", folderController.createFolder);
 router.get("/new", (req, res) => res.render("folder/folder-create-form"));
+router.post("/new", folderController.createFolder);
+
+router.get("/:id/edit", folderController.editFolder);
+router.post("/:id/update", folderController.updateFolder);
 module.exports = router;
