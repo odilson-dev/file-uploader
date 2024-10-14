5710a22c701e91a98f39d162a071d53c6109283b
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-10-13 22:39:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,6 @@ const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
-async function main() {
-  // ... you will write your Prisma Client queries here
-  const allUsers = await prisma.user.findMany();
-  console.log(allUsers);
-}
-
 main()
   .then(async () => {
     await prisma.$disconnect();
